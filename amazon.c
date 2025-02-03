606278d43d3f66dd8e45cccde47d9048c84aeeda
VamshiKrishna-K <24071A05Q6@vnrvjiet.in>
2025-02-03 20:25:00 +0530

    Update amazon.c

--- amazon.c
+++ amazon.c
@@ -11,12 +11,12 @@ p=fopen("a1z.txt","r");
   printf("\nEnter which type of product you want  ");
   scanf("%c",&ch);clrscr();
   k=fopen("a2z.txt","r");
-  while((c=getc(k))1=EOF)
+  while((c=getc(k))!=EOF)
     {
     if(c==ch)
     {
       printf("\n%d",i);
-      while((c=getc(k))1='.')
+      while((c=getc(k))!='.')
         printf("%c",c);
       break;
         }
