e873680be0840f031261e273342233915e9c376d
VamshiKrishna-K <24071A05Q6@vnrvjiet.in>
2025-02-03 19:32:37 +0530

    Update amazon.c

--- amazon.c
+++ amazon.c
@@ -1,5 +1,25 @@
 #include<stdio.h>
+#include<conio.h>
 void main()
 {
-printf("Hello world");
+char c,ch;
+FILE *p,*k; clrscr();
+p=fopen("a1z.txt","r");
+  while((c=getc(p)!=EOF)
+    printf("%c",c);
+  fcloseall();
+  printf("\nEnter which type of product you want  ");
+  scanf("%c",&ch);clrscr();
+  k=fopen("a2z.txt","r");
+  while((c=getc(k))1=EOF)
+    {
+    if(c==ch)
+    {
+      printf("\n%d",i);
+      while((c=getc(k))1='.')
+        printf("%c",c);
+      break;
+        }
+    } fcloseall();
+  getch();
 }
